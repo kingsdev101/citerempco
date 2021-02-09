6b633cf0d90cee19b8e512ccae73c515aab8f01f
kingsdev101 <brianhiddenfeelngs@gmail.com>
2021-02-09 09:14:32 +0000

    first commits

---
+++ resources/views/layouts/custome.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/fix.css')}}">
+    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+    <!-- mobiles -->
+    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+    <link rel="stylesheet" href="{{asset('css/mobile/welcome.css')}}">
+</head>
+
+<body>
+    <!-- 
+|--------------------------------------------------------------------------
+| header
+|--------------------------------------------------------------------------
+| -->
+    <header>
+        <div class="box">
+            <div class="logo-image"><img src="{{asset('storage/project_mages/king.ico')}}" alt="logo"></div>
+            <div class="logoname"> <span>CITEREMPCO</span> <small>Isulan</small></div>
+        </div>
+        <div class="box">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Log-in</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- 
+|--------------------------------------------------------------------------
+| content
+|--------------------------------------------------------------------------
+| -->
+
+    <div class="container-fluide">
+
+        @yield('content')
+
+
+    </div>
+    <!-- 
+|--------------------------------------------------------------------------
+| footer area
+|--------------------------------------------------------------------------
+| -->
+    <footer>
+        <div class="footer-container row">
+            <div class="fbox-wrapper col-lg-3 col-md-6 col-sm-6">
+                <h2>Useful links</h2>
+                <div class="fbox-content ">
+                <div class="flink-wrapper">
+                    <ul class="flinks">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sign-Up</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Log in</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="fbox-wrapper col-lg-3 col-md-6 col-sm-6">
+                <h2>Latest Post</h2>
+                <div class="fbox-content">
+                    <p>
+                        Latest loan of Central Isulan Teachers,
+                        Employees and Retirees Multi - Purpose Cooperative is for the employee
+                    </p>
+                </div>
+            </div>
+
+            <div class="fbox-wrapper col-lg-3 col-md-6 col-sm-6">
+                <h2>Business Hours</h2>
+                <div class="fbox-content">
+                    <p>
+                        Loan transaction in the office of
+                        Citerempco is available at.<sup></sup>
+                    </p>
+                    <p class="time">Mon to Fri : 8:00 am to 5:00 pm</p>
+                </div>
+            </div>
+
+            <div class="fbox-wrapper col-lg-3 col-md-6 col-sm-6">
+                <h2>Tags</h2>
+                <div class="fbox-content">
+                    <div class="flink-wrapper">
+                        <ul class="ftags">
+                            <li><a href="#">Map</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Post</a></li>
+                            <li><a href="#">Clients</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="powerd"> Powerd by: J7. IT - solutions</div>
+    </footer>
+</body>
+
+</html>
---
+++ resources/views/welcome/welcome.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.custome')
+
+@section('content')
+<div class="about-us">
+
+    <!-- 
+|---------------------------------------------
+| image
+|--------------------------------------------- -->
+    <div class="about-us-image-wrapper">
+        <div class="about-us-image">
+        </div>
+    </div>
+    <!-- 
+|---------------------------------------------
+| about us with lines
+|--------------------------------------------- -->
+
+    <div class="citerempco">
+        <h2> CITEREMPCO</h2>
+    </div>
+
+    <!-- 
+|---------------------------------------------
+| about us content
+|--------------------------------------------- -->
+
+    <div class="about-us-content">
+        <div class="about-us-content">
+        </div>
+        <div class="about-text">
+            <h2>About uS</h2>
+            <p>
+                CITEREMPCO is developed and managed by Inclusive Financial Technologies as an online platform to connect
+                borrowers who need access to a financial institution to fund their goals and lenders who want to grow
+                their wealth through intelligible investing.
+            </p>
+        </div>
+
+        <div class="about-us-box">
+            <div>
+                <h2>Our Mission</h2>
+                <p>Provide various lending and livelihood programs, promote savings mobilization. Conduct continuous
+                    education and trainings, attend values formation programs, and operate in the principles of
+                    transparency of information and operation. Provide social and health services, scholarship / study
+                    grants, and participate in the clean and green programs and religious activities in the community.
+                    Strengthen cooperative linkages to LGU's, NGO's and GO's.</p>
+            </div>
+            <div class="about-us-box">
+                <div>
+                    <h2>Our Vision</h2>
+                    <p>The Central Isulan Teachers, Employees, and Retirees Multi Purpose Cooperative (CITERMPCO) with
+                        committed, competent, and concerned leaders extending assistance and services to members in a
+                        God - fearing, progressive and environment - friendly community.</p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+    @endsection
