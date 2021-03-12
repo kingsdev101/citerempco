a1bad4835082b4de1b20b90f075239985f01bc35
kingsdev101 <brianhiddenfeelngs@gmail.com>
2021-03-12 08:09:47 +0000

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Url
     }
 
     /**
@@ -23,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV') !== 'local'){
-            URL::forceScheme('https');
-        }
+        
     }
 }
